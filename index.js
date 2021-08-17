c4d4da88a9e8f4dc3621a9eee72290ad699056e9
victoriatrinita <victoriapardede@gmail.com>
2021-08-17 20:38:59 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Success");
+});
