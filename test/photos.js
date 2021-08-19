ea0856f2097c08b8de83e527271d113a4f511a49
victoriatrinita <victoriapardede@gmail.com>
2021-08-19 16:38:53 +0700

    test: add unit test for GET photos by tag route

--- test/photos.js
+++ test/photos.js
@@ -28,4 +28,29 @@ describe("Photos API", () => {
       done();
     });
   });
+
+  describe("GET /:tag", () => {
+    it("It should GET all the photos by tag", (done) => {
+      const tag = "tag";
+
+      chai
+        .request(server)
+        .get("/" + tag)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.response.length.should.be.eq(20);
+          response.body.response[0].should.have.property("title");
+          response.body.response[0].should.have.property("link");
+          response.body.response[0].should.have.property("media");
+          response.body.response[0].should.have.property("date_taken");
+          response.body.response[0].should.have.property("description");
+          response.body.response[0].should.have.property("published");
+          response.body.response[0].should.have.property("author");
+          response.body.response[0].should.have.property("author_id");
+          response.body.response[0].should.have.property("tags");
+        });
+      done();
+    });
+  });
 });
