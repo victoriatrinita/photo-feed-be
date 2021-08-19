c7baada052991d175535a145087a524d06d02660
victoriatrinita <victoriapardede@gmail.com>
2021-08-19 11:59:20 +0700

    feat: enable CORS

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/", (req, res) => {
