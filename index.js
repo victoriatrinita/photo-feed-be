d8932162cec89bf9f31153d97530bea6e27c7bb0
victoriatrinita <victoriapardede@gmail.com>
2021-08-19 11:54:22 +0700

    feat: add GET photos endpoint

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const axios = require("axios");
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/", (req, res) => {
-  res.send("Success");
+  axios
+    .get(
+      "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"
+    )
+    .then((response) => res.send({ success: true, response: response.data }))
+    .catch((error) => res.send({ success: false, message: error.message }));
 });
